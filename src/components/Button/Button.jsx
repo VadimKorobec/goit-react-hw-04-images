732c9169e543c6b587e3cf0e2a48b67967b5c199
VadimKorobec <_s_e_t__@ukr.net>
2023-01-29 15:13:08 +0200

    Create Button.jsx

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+export const Button = ({ onLoadMore }) => {
+  return (
+    <button className="Button" type="button" onClick={onLoadMore}>
+      Load more
+    </button>
+  );
+};
+
+Button.propTypes = {
+  onLoadMore: PropTypes.func.isRequired,
+};
