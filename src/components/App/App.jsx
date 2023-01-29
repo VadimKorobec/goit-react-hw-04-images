58585bdf58c9ffe1b43dfbcb41160482a025cdac
VadimKorobec <_s_e_t__@ukr.net>
2023-01-29 16:03:15 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,65 +6,68 @@ import { fetchImages } from 'services/pixabayApi';
 import { Wrapper } from './App.styled';
 import { ToastContainer } from 'react-toastify';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const App = () => {
-  const [query, setQuery] = useState('')
-  const [page, setPage] = useState(1)
-  const [images, setImages] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [totalImages, setTotalImages] = useState(0)
-  const [error, setError] = useState(null)
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalImages, setTotalImages] = useState(0);
+  const [error, setError] = useState(null);
 
- 
-
-  const componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      fetchImages(query, page)
-        .then(resp => {
-          this.setState(prev => ({
-            images:
-              page === 1 ? [...resp.hits] : [...prev.images, ...resp.hits],
-            totalImages: resp.totalHits,
-          }));
-        })
-        .finally(() => {
-          this.setState({ isLoading: false });
-        });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await fetchImages(query, page);
+        if (resp) {
+          if (!resp.totalHits) {
+            throw new Error('Bad query');
+          }
+          setImages(prevImages =>
+            page === 1 ? [...resp.hits] : [...prevImages, ...resp.hits]
+          );
+          setTotalImages(resp.totalHits);
+          setError(null);
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (query) {
+      fetchData();
     }
-  }
+  }, [page, query]);
 
-const  handleSubmit = query => {
+  const handleSubmit = query => {
     setQuery(query);
     setIsLoading(true);
     setPage(1);
   };
 
   const handleLoadMore = () => {
-  setIsLoading(true)
+    setIsLoading(true);
     setPage(prevPage => prevPage + 1);
   };
 
-const   renderButtonOrLoader = () => {
+  const renderButtonOrLoader = () => {
     return isLoading ? (
       <Loader />
     ) : (
-      !!images.length &&
-        images.length < totalImages && (
-          <Button onLoadMore={this.handleLoadMore} />
-        )
+      !!images.length && images.length < totalImages && (
+        <Button onLoadMore={handleLoadMore} />
+      )
     );
   };
-  
-    
 
-    return (
-      <>
-        <Searchbar onSubmit={handleSubmit} />
-        <ImageGallery images={images} />
-        {<Wrapper>{renderButtonOrLoader()}</Wrapper>}
-        <ToastContainer theme="colored" position="top-right" autoClose={3000} />
-      </>
-    );
-  }
-
+  return (
+    <>
+      <Searchbar onSubmit={handleSubmit} />
+      <ImageGallery images={images} />
+      {!error && <Wrapper>{renderButtonOrLoader()}</Wrapper>}
+      <ToastContainer theme="colored" position="top-right" autoClose={3000} />
+    </>
+  );
+};
